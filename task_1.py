9cbfd71ba91266dfbb082ff54a599043c4d3a074
oleksii.k <oleksii.k@appsflyer.com>
2025-05-06 23:01:31 +0200

    upd task 1

--- task_1.py
+++ task_1.py
@@ -1,6 +1,3 @@
-import binascii
-
-
 class BloomFilter:
     def __init__(self, size, num_hashes):
         self.size = size
@@ -8,20 +5,20 @@ class BloomFilter:
         self.bit_array = [0] * size
 
     def add(self, item):
-        seq = int(''.join(str(ord(c)) for c in str(item)))
-        index = seq // len(item) % self.size
-        self.bit_array[index] = 1
+        for i in range(1, self.num_hashes + 1):
+            index = int(''.join(str(ord(c)) for c in str(item))) // i // len(str(item)) % self.size
+            self.bit_array[index] = 1
 
     def contains(self, item):
-        seq = int(''.join(str(ord(c)) for c in str(item)))
-        index = seq // len(item) % self.size
-        if self.bit_array[index] == 0:
-            return False
+        for i in range(1, self.num_hashes + 1):
+            index = int(''.join(str(ord(c)) for c in str(item))) // i // len(str(item)) % self.size
+            if self.bit_array[index] == 0:
+                return False
         return True
 
 
 def check_password_uniqueness(filter_, new_passwords):
-    return {p: filter_.contains(p) for p in new_passwords}
+    return {str(p): filter_.contains(p) for p in new_passwords}
 
 
 if __name__ == "__main__":
@@ -29,12 +26,12 @@ if __name__ == "__main__":
     bloom = BloomFilter(size=1000, num_hashes=3)
 
     # Додавання існуючих паролів
-    existing_passwords = ["password123", "admin123", "qwerty123"]
+    existing_passwords = ["password123", "admin123", "qwerty123", True]
     for password in existing_passwords:
         bloom.add(password)
 
     # Перевірка нових паролів
-    new_passwords_to_check = ["password123", "newpassword", "admin123", "guest"]
+    new_passwords_to_check = ["password123", "newpassword", "admin123", "guest", None, [12, 'foo', None]]
     results = check_password_uniqueness(bloom, new_passwords_to_check)
 
     # Виведення результатів
